60c4d6d524ea6d58311c9344f23d1580afa279b0
Swara <gujarathiswaraa@gmail.com>
2026-06-11 00:16:27 +0530

    Add temporarily all domains to CORS

--- backend/main.py
+++ backend/main.py
@@ -15,11 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://panic-planner.vercel.app",
-        "https://panic-planner-swarovsky1907s-projects.vercel.app/"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
